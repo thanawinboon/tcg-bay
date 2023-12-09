86b1123e69ec28fb5a0b719e1897abd30f3e4356
thanawinboon <pokepat27@gmail.com>
2023-12-09 08:48:45 +0700

    added search functionality

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,5 +1,41 @@
 <script setup>
+import { ref } from 'vue'
 import CardsDisplay from '@/components/CardsDisplay.vue'
+import { db } from '@/js/firebase.js'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
+// TODO: Show some fancy TCG wallpaper plus website's name ???
+
+const cards = ref([]);
+const searchInput = ref('')
+
+
+function searchCards() {
+  console.log('Searching for cards')
+  const cardsCollection = collection(db, 'cards');
+  const queryCards = query(cardsCollection, where('name', '>=', searchInput.value), where('name', '<=', `${searchInput.value}\uf8ff`));
+  onSnapshot(queryCards, (querySnapshot) => {
+    try {
+      let cardsSnapshot = []
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data())
+        let card = {
+          id: doc.id,
+          category: doc.data().category,
+          name: doc.data().name,
+          note: doc.data().note,
+          time: doc.data().time,
+          owner: doc.data().owner
+        }
+        cardsSnapshot.push(card)
+      })
+      cards.value = cardsSnapshot
+      console.log('Cards retrieved:', cards.value)
+    } catch (error) {
+      console.error('Error fetching cards:', error)
+    }
+  })
+}
+
 // TODO: Add search page 
 // - Display all cards showing each one's image, name, owner's username (AND RATING), category
 // - Has a textbox for input
@@ -10,15 +46,15 @@ import CardsDisplay from '@/components/CardsDisplay.vue'
   <div id="search">
     <div class="search-header">
       <h2>Search</h2>
-      <form class="search-container">
-        <input type="text" id="search-bar" placeholder="Enter card name here...">
+      <form class="search-container" @submit.prevent="searchCards">
+        <input v-model="searchInput" type="text" id="search-bar" placeholder="Enter card name here...">
         <button>
           <img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png" alt="search">
         </button>
       </form>
     </div>
     <div class="search-body">
-      <CardsDisplay/>
+      <CardsDisplay :cards="cards"/>
     </div>
   </div>
 </template>
