54fecf794342dc65c050857fca1473cef05f4d6c
thanawinboon <pokepat27@gmail.com>
2023-12-09 08:21:41 +0700

    made cardsdisplay component reusable

--- src/components/CardsDisplay.vue
+++ src/components/CardsDisplay.vue
@@ -1,39 +1,15 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { db } from '@/js/firebase.js'
-import { collection, onSnapshot } from 'firebase/firestore'
-
+import { onMounted } from 'vue'
 import CardItem from '@/components/CardItem.vue'
 
-const cards = ref([])
-
-async function getCards() {
-  onSnapshot(collection(db, 'cards'), (querySnapshot) => {
-    try {
-      let cardsSnapshot = []
-      querySnapshot.forEach((doc) => {
-        console.log(doc.id, ' => ', doc.data())
-        let card = {
-          id: doc.id,
-          category: doc.data().category,
-          name: doc.data().name,
-          note: doc.data().note,
-          time: doc.data().time,
-          owner: doc.data().owner
-        }
-        cardsSnapshot.push(card)
-      })
-      cards.value = cardsSnapshot
-      console.log('Cards retrieved:', cards.value)
-    } catch (error) {
-      console.error('Error fetching cards:', error)
-    }
-  })
-}
+defineProps({
+  cards: {
+    required: true
+  }
+})
 
 onMounted(async () => {
   console.log('Connecting to Firebase')
-  await getCards()
 })
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,38 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import CardsDisplay from '@/components/CardsDisplay.vue'
-// TODO: Show some fancy TCG wallpaper plus website's name ??
+import { db } from '@/js/firebase.js'
+import { collection, onSnapshot } from 'firebase/firestore'
+// TODO: Show some fancy TCG wallpaper plus website's name ???
+
+const cards = ref([]);
+async function getCards() {
+  onSnapshot(collection(db, 'cards'), (querySnapshot) => {
+    try {
+      let cardsSnapshot = []
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data())
+        let card = {
+          id: doc.id,
+          category: doc.data().category,
+          name: doc.data().name,
+          note: doc.data().note,
+          time: doc.data().time,
+          owner: doc.data().owner
+        }
+        cardsSnapshot.push(card)
+      })
+      cards.value = cardsSnapshot
+      console.log('Cards retrieved:', cards.value)
+    } catch (error) {
+      console.error('Error fetching cards:', error)
+    }
+  })
+}
+
+onMounted(async () => {
+  await getCards()
+})
 
 </script>
 
@@ -11,7 +43,7 @@ import CardsDisplay from '@/components/CardsDisplay.vue'
       <p>Home page is where the heart of the card is</p>
     </div>
     <div>
-      <CardsDisplay/>
+      <CardsDisplay :cards="cards"/>
     </div>
   </div>
 </template>
