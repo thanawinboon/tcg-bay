75c86a989fdde3e06015dfaf9ceef8a9938e9fab
thanawinboon <pokepat27@gmail.com>
2023-12-10 13:30:16 +0700

    added something on about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>The project is done by</h2>
+    <ul>
+      <li>Thanawin Boonpojanasoontorn 6280163</li>
+      <li>Adrian Ang 6380989</li>
+    </ul>
   </div>
 </template>
 
@@ -8,7 +13,7 @@
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
+    display: block;
     align-items: center;
   }
 }
